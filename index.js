87606f783be3da03269a94d220cdd83ea7b940ff
habibou9 <danaoutah@yahoo.com>
2019-04-13 21:21:46 -0400

    add involved page and update server
    add involved page and update server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
-var path = require('path');
+//var path = require('path');
+var path = __dirname + '/views/';
 
 app.set ('view engine','ejs');
 //app.use('/views',express.static('views'))
@@ -9,9 +10,50 @@ app.set ('view engine','ejs');
 
 //app.get('/', (req, res) => res.render('store'));
 
-app.get('/', function(req, res){
+app.use(express.static(__dirname + '/ressources'));
 
+app.get('/', function(req, res){
+  console.log(req.url)
+res.render(path + 'index');
+});
+
+app.get('/Involved', function(req, res){
+  console.log(req.url)
+res.render('involved');
+});
+
+app.get('/Newsletter', function(req, res){
+  console.log(req.url)
+res.render('newsletter');
+});
+
+app.get('/Registration', function(req, res){
+  console.log(req.url)
+res.render('registrationPage');
+});
+
+app.get('/Signin', function(req, res){
+  console.log(req.url)
+res.render('signin');
+});
+
+app.get('/Store', function(req, res){
+  console.log(req.url)
 res.render('store');
 });
+
+app.get('/InvolvedAsDonor', function(req, res){
+  console.log(req.url)
+res.render('involvedAsDonor');
+});
+
+app.get('/InvolvedAsVolunteer', function(req, res){
+  console.log(req.url)
+res.render('involvedAsVolunteer');
+});
+
+
+
+
 
 app.listen(3000, () => console.log(`Meal Share app listening on port 3000!`));
