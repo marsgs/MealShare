771871d18bdba59f904930cbb4440af15e4f41c1
habibou9 <danaoutah@yahoo.com>
2019-05-06 20:18:08 -0400

    implement database and login

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express')
 var app = express()
 //var path = require('path');
 var path = __dirname + '/views/';
-
+//const path = require('path');
 app.set ('view engine','ejs');
+var mongoose = require('mongoose');
+var itemDB = require('/Users/habibmda/Documents/GitHub/MealShare/utility/itemDB.js');
+var session = require('express-session');
+app.use(session({secret: "nbad session secret"}));
+const bodyParser = require('body-parser');
 //app.use('/views',express.static('views'))
 //app.use('/ressources',express.static('ressources'))
 //app.use('/assets', express.static(__dirname +'/../resources'));
@@ -11,6 +16,8 @@ app.set ('view engine','ejs');
 //app.get('/', (req, res) => res.render('store'));
 
 app.use(express.static(__dirname + '/ressources'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res){
   console.log(req.url)
@@ -32,9 +39,26 @@ app.get('/Registration', function(req, res){
 res.render('registrationPage');
 });
 
-app.get('/Signin', function(req, res){
+app.get('/Signin*', function(req, res){
+
   console.log(req.url)
-res.render('signin');
+
+  res.render("signin");
+
+});
+
+app.post('/signInfo*', function(req, res){
+
+  console.log(req.url)
+  var data = itemDB.getItems(0);
+  console.log(data[0]);
+  console.log(req.body);
+  if(req.body.username == data[0].userName && req.body.password == data[0].password){
+    res.render("profileIndex");
+  }else {
+    res.render("signin");
+  }
+
 });
 
 app.get('/Store', function(req, res){
@@ -64,7 +88,7 @@ res.render('scheduleDroppOff');
 
 app.get('/account', function(req, res){
   console.log(req.url)
-res.render('account');
+res.sendFile('account.html');
 });
 
 app.get('/donationRecord', function(req,res){
--- utility/itemDB.js
+++ utility/itemDB.js
@@ -1,12 +1,13 @@
-var user1Donor = { firstName: "Marc", lastName:"Maden",dateOfBirth:"05/19/1956",Email:"mmaden@gmail.com", phone:"704-212-4567"}
-var user2Volunteer = { firstName: "Lucie", lastName:"Monroe", address:"212 troy street, columbia, NC",Email:"lmonroe@gmail.com",phone:"704-212-4567", age:"13" }
+var user1Donor = { firstName: "Marc", lastName:"Maden",userName:"mmaden", password:"den23",dateOfBirth:"05/19/1956",Email:"mmaden@gmail.com", phone:"704-212-4567"};
+var user2Volunteer = { firstName: "Lucie", lastName:"Monroe",userName:"lmonroe",password:"luce12", address:"212 troy street, columbia, NC",Email:"lmonroe@gmail.com",phone:"704-212-4567", age:"13" };
 
-var item={user1Donor, user2Volunteer}
+var item=[user1Donor, user2Volunteer];
 
 var util = require('util');
+
 var items = function(elements){
   this.elements = elements;
 }
 module.exports.getItems = function(){
-  return items
+  return item;
 }
